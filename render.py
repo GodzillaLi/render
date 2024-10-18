8ce8f603fabf8f4e2083b83cf34629eacb5a857b
Jing Li <1246494002@qq.com>
2024-10-18 17:35:18 +0800

    Create render.py

---
+++ render.py
@@ -0,0 +1,232 @@
+import argparse
+import math
+import os
+import random
+import sys
+import time
+import urllib.request
+from typing import List, Tuple
+
+import bpy
+from mathutils import Vector, Matrix
+import numpy as np
+import json
+
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "--object_path",
+    type=str,
+    required=True,
+    help="Path to the object file",
+)
+parser.add_argument("--output_dir", type=str, default="./views")
+parser.add_argument(
+    "--engine", type=str, default="BLENDER_EEVEE", choices=["CYCLES", "BLENDER_EEVEE"]
+)
+parser.add_argument("--num_images", type=int, default=32)
+parser.add_argument("--camera_dist", type=float, default=0.5)
+
+argv = sys.argv[sys.argv.index("--") + 1 :]
+args = parser.parse_args(argv)
+
+context = bpy.context
+scene = context.scene
+render = scene.render
+
+render.engine = args.engine
+render.image_settings.file_format = "PNG"
+render.image_settings.color_mode = "RGBA"
+render.resolution_x = 512
+render.resolution_y = 512
+render.resolution_percentage = 100
+
+if render.engine == "CYCLES":
+    scene.cycles.device = "GPU"
+    scene.cycles.samples = 32
+    scene.cycles.diffuse_bounces = 1
+    scene.cycles.glossy_bounces = 1
+    scene.cycles.transparent_max_bounces = 3
+    scene.cycles.transmission_bounces = 3
+    scene.cycles.filter_width = 0.01
+    scene.cycles.use_denoising = True
+    scene.render.film_transparent = True
+
+
+def sample_point_on_sphere(radius: float) -> Tuple[float, float, float]:
+    theta = random.random() * 2 * math.pi
+    phi = math.acos(2 * random.random() - 1)
+    return (
+        radius * math.sin(phi) * math.cos(theta),
+        radius * math.sin(phi) * math.sin(theta),
+        radius * math.cos(phi),
+    )
+
+
+def add_lighting() -> None:
+    # delete the default light
+    bpy.data.objects["Light"].select_set(True)
+    bpy.ops.object.delete()
+    # add HDRI lighting
+    bpy.ops.object.light_add(type='SUN', radius=1)
+    sun = bpy.data.objects['Sun']
+    sun.location = (0, 0, 10)
+    sun.data.energy = 5
+    
+
+def reset_scene() -> None:
+    """Resets the scene to a clean state."""
+    # delete everything that isn't part of a camera or a light
+    for obj in bpy.data.objects:
+        if obj.type not in {"CAMERA", "LIGHT"}:
+            bpy.data.objects.remove(obj, do_unlink=True)
+    # delete all the materials
+    for material in bpy.data.materials:
+        bpy.data.materials.remove(material, do_unlink=True)
+    # delete all the textures
+    for texture in bpy.data.textures:
+        bpy.data.textures.remove(texture, do_unlink=True)
+    # delete all the images
+    for image in bpy.data.images:
+        bpy.data.images.remove(image, do_unlink=True)
+
+
+# load the glb model
+def load_object(object_path: str) -> None:
+    """Loads a glb model into the scene."""
+    if object_path.endswith(".glb"):
+        bpy.ops.import_scene.gltf(filepath=object_path, merge_vertices=True)
+    elif object_path.endswith(".fbx"):
+        bpy.ops.import_scene.fbx(filepath=object_path)
+    else:
+        raise ValueError(f"Unsupported file type: {object_path}")
+
+
+def scene_bbox(single_obj=None, ignore_matrix=False):
+    bbox_min = (math.inf,) * 3
+    bbox_max = (-math.inf,) * 3
+    found = False
+    for obj in scene_meshes() if single_obj is None else [single_obj]:
+        found = True
+        for coord in obj.bound_box:
+            coord = Vector(coord)
+            if not ignore_matrix:
+                coord = obj.matrix_world @ coord
+            bbox_min = tuple(min(x, y) for x, y in zip(bbox_min, coord))
+            bbox_max = tuple(max(x, y) for x, y in zip(bbox_max, coord))
+    if not found:
+        raise RuntimeError("no objects in scene to compute bounding box for")
+    return Vector(bbox_min), Vector(bbox_max)
+
+
+def scene_root_objects():
+    for obj in bpy.context.scene.objects.values():
+        if not obj.parent:
+            yield obj
+
+
+def scene_meshes():
+    for obj in bpy.context.scene.objects.values():
+        if isinstance(obj.data, (bpy.types.Mesh)):
+            yield obj
+
+
+def normalize_scene():
+    bbox_min, bbox_max = scene_bbox()
+    scale = 1 / max(bbox_max - bbox_min)
+    for obj in scene_root_objects():
+        obj.scale = obj.scale * scale
+    # Apply scale to matrix_world.
+    bpy.context.view_layer.update()
+    bbox_min, bbox_max = scene_bbox()
+    offset = -(bbox_min + bbox_max) / 2
+    for obj in scene_root_objects():
+        obj.matrix_world.translation += offset
+    bpy.ops.object.select_all(action="DESELECT")
+
+
+def setup_camera():
+    if 'Camera' not in scene.objects:
+        bpy.ops.object.camera_add()
+    cam = scene.objects["Camera"]
+    cam.data.lens = 35
+    cam.data.sensor_width = 32
+    cam_constraint = cam.constraints.new(type="TRACK_TO")
+    cam_constraint.track_axis = "TRACK_NEGATIVE_Z"
+    cam_constraint.up_axis = "UP_Y"
+    return cam, cam_constraint
+
+
+def save_images(object_file: str) -> None:
+    """Saves rendered images of the object in the scene."""
+    os.makedirs(args.output_dir, exist_ok=True)
+    output_camera_params = []
+    reset_scene()
+    # load the object
+    load_object(object_file)
+    object_uid = os.path.basename(object_file).split(".")[0]
+    normalize_scene()
+    add_lighting()
+
+    cam, cam_constraint = setup_camera()
+    # create an empty object to track
+    empty = bpy.data.objects.new("Empty", None)
+    scene.collection.objects.link(empty)
+    cam_constraint.target = empty
+    
+    fov = random.uniform(15, 60)
+    elevation = random.uniform(0, 30) / 180 * math.pi  # Convert to radians
+    
+    cam.data.angle = math.radians(fov)
+    for i in range(args.num_images):
+        # set the camera position
+        theta = (i / args.num_images) * math.pi * 2
+        point = (
+            args.camera_dist * math.sin(elevation) * math.cos(theta),
+            args.camera_dist * math.sin(elevation) * math.sin(theta),
+            args.camera_dist * math.cos(elevation),
+        )
+        cam.location = point
+        # render the image
+        render_path = os.path.join(args.output_dir, object_uid, f"{i:03d}.png")
+        scene.render.filepath = render_path
+        bpy.ops.render.render(write_still=True)
+        
+        # Save camera parameters
+        camera_matrix = cam.matrix_world.normalized()
+        camera_params = {'rotation': camera_matrix.to_quaternion().normalized().to_tuple()}
+        output_camera_params.append(camera_matrix.to_list())
+    
+    with open(os.path.join(args.output_dir, object_uid, "camera_params.json"), 'w') as json_file:
+        json.dump(output_camera_params, json_file, indent=2)
+
+
+def download_object(object_url: str) -> str:
+    """Download the object and return the path."""
+    uid = object_url.split("/")[-1].split(".")[0]
+    tmp_local_path = os.path.join("tmp-objects", f"{uid}.glb" + ".tmp")
+    local_path = os.path.join("tmp-objects", f"{uid}.glb")
+    # wget the file and put it in local_path
+    os.makedirs(os.path.dirname(tmp_local_path), exist_ok=True)
+    urllib.request.urlretrieve(object_url, tmp_local_path)
+    os.rename(tmp_local_path, local_path)
+    # get the absolute path
+    local_path = os.path.abspath(local_path)
+    return local_path
+
+
+if __name__ == "__main__":
+    try:
+        start_i = time.time()
+        if args.object_path.startswith("http"):
+            local_path = download_object(args.object_path)
+        else:
+            local_path = args.object_path
+        save_images(local_path)
+        end_i = time.time()
+        print("Finished", local_path, "in", end_i - start_i, "seconds")
+        # delete the object if it was downloaded
+        if args.object_path.startswith("http"):
+            os.remove(local_path)
+    except Exception as e:
+        print("Failed to render", args.object_path)
+        print(e)
